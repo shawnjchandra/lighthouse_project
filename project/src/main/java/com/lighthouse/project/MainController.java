5b3dbc85bd3a9bc282a5d44ae24439d36734c94f
Shawn <myking123321@gmail.com>
2024-11-18 22:47:51 +0700

    few updates

--- project/src/main/java/com/lighthouse/project/MainController.java
+++ project/src/main/java/com/lighthouse/project/MainController.java
@@ -55,7 +55,7 @@ public class MainController {
 
         return "editData";
     }
-
+    
     @GetMapping("/kelola-jadwal")
     public String kelolaJadwal(Model model) {
 
@@ -64,10 +64,30 @@ public class MainController {
 
     @GetMapping("/pengawasan-cico")
     public String pengawsanCICO(Model model) {
-
+        
         return "pengawasanCICO";
     }
     
+    @GetMapping("/pemesanan")
+    public String pesan(Model model) {
+
+        return "pemesanan";
+    }
+    @GetMapping("/pembayaran")
+    public String pembayaran(Model model) {
+
+        return "pembayaran";
+    }
+    @GetMapping("/pengawasan-unit")
+    public String pengawasanUnit(Model model) {
+
+        return "pengawasanUnit";
+    }
+    @GetMapping("/pelaporan-utilitas")
+    public String pelaporanUtilitas(Model model) {
+
+        return "pelaporanUtilitas";
+    }
     // Tinggal Pelaporan Utilitas dan Pengawasan Unit
     
     
