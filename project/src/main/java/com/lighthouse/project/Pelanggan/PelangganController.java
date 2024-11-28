d5a7035c158a01570cf01fbba70429c075b62c78
Ringo-K <lintangkastarae@gmail.com>
2024-11-28 23:32:04 +0700

    Update pada fitur pencarian pelanggan, sekarang card akan memunculkan unit yang ada di database

--- project/src/main/java/com/lighthouse/project/Pelanggan/PelangganController.java
+++ project/src/main/java/com/lighthouse/project/Pelanggan/PelangganController.java
@@ -1,5 +1,13 @@
 package com.lighthouse.project.Pelanggan;
 
+import java.util.List;
+
+import com.lighthouse.project.Tower.TowerUnitJDBC;
+import com.lighthouse.project.Tower.TowerUnitModel;
+// import com.lighthouse.project.Tower.TowerUnitRepo;
+// import com.lighthouse.project.Tower.UnitModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/ptyp")
 public class PelangganController {
+
+    // @Autowired
+    // private TowerUnitRepo towerRepo;
+
+    @Autowired
+    private TowerUnitJDBC towerUnitJDBC;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -35,7 +49,10 @@ public class PelangganController {
     }
     @GetMapping("/pencarian")
     public String penc(Model model) {
-
+        // List<TowerUnitModel> units = towerRepo.findAllUnitJoinTowers();
+        List<TowerUnitModel> units = towerUnitJDBC.findAllUnitJoinTowers();
+        // List<UnitModel> units = towerRepo.findAllUnits();
+        model.addAttribute("units", units);
         return "pencarian";
     }
 
@@ -50,5 +67,5 @@ public class PelangganController {
 
         return "pembayaran";
     }
-    
+
 }
